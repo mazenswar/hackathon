fbbfa0d6e2cded52f121c29c41e9ecc5c1e43ad2
github username <github email address>
2019-04-13 13:00:17 -0400

    Created Form.jsx

---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Form = (props) => {
+  return (
+    <form>
+      <input type="text" />
+      <button type="button">Submit</button>
+    </form>
+  )
+}
+
+export default Form
